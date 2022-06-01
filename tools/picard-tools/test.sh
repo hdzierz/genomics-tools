0e70d0ff87309945cec40b90f1d5168454cb6299
swbisset <threedegreeswest@hotmail.com>
2022-06-01 13:07:09 +1200

    Fixed typo in test.sh

--- tools/picard-tools/test.sh
+++ tools/picard-tools/test.sh
@@ -3,7 +3,7 @@
 # Try picard-tools FilterVcf  and see if it is returns 'JavOpt' .
 # If it does, then output GFANZ_TEST_RESULTS=OK. If it does not, then output
 # GFANZ_TEST_RESULTS=ERROR.
-/usr/bin/PicardCommandLine FilterVcf | grep 'javOpt' >> /dev/null
+/usr/bin/PicardCommandLine FilterVcf | grep 'JavOpt' >> /dev/null
 
 if [ $? -eq 0 ]; then
   echo "GFANZ_TEST_RESULTS=OK"
