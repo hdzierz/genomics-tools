c0cbcb04d4b65623e89cd4031a08dd9f1655c00e
Rob Elshire <rob@elshiregroup.co.nz>
2022-06-20 17:04:26 +1200

    Corrected path for conda binary.

--- tools/ipyrad/install.sh
+++ tools/ipyrad/install.sh
@@ -29,4 +29,4 @@ apt-get remove -y curl
 
 # Install ipyrad
 
-/opt/miniconda2/bin/conda install ipyrad -c conda-forge -c bioconda
+/opt/conda/condabin/conda install ipyrad -c conda-forge -c bioconda
