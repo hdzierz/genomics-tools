0ebaa5747d4551d7d8ae4668cfbab1019f171160
Rob Elshire <rob@elshiregroup.co.nz>
2022-06-20 18:41:40 +1200

    Refactored the miniconda install entirely.

--- tools/ipyrad/install.sh
+++ tools/ipyrad/install.sh
@@ -3,36 +3,18 @@
 # This program is to set up a docker image for the ipyrad version 0.9.83
 # It assumes a Debian 11 base image has been used for the container.
 
-# Update package database and install curl, python2.7 & gpg
+# Update package database and install curl
 
 apt-get update
-apt-get install -y curl python2.7 gpg
-
-# Remove python
-
-#apt-get remove --purge python3-minimal
-
-# List installed packages so we can identify python3 and remove it
-
-dpkg --get-selections
+apt-get install -y curl 
 
 # Install mini-conda
-# Install our public GPG key to trusted store
-curl https://repo.anaconda.com/pkgs/misc/gpgkeys/anaconda.asc | gpg --dearmor > conda.gpg
-install -o root -g root -m 644 conda.gpg /usr/share/keyrings/conda-archive-keyring.gpg
+cd /tmp
+wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh
 
-# Check whether fingerprint is correct (will output an error message otherwise)
-gpg --keyring /usr/share/keyrings/conda-archive-keyring.gpg --no-default-keyring --fingerprint 34161F5BF5EB1D4BFBBB8F0A8AEB4F8B29D82806
-
-# Add our Debian repo
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/conda-archive-keyring.gpg] https://repo.anaconda.com/pkgs/misc/debrepo/conda stable main" > /etc/apt/sources.list.d/conda.list
-
-# Install it!
-apt-get update
-apt-get install -y conda
+bash Miniconda3-py39_4.12.0-Linux-x86_64.sh -b -p /opt/conda
 
 # Remove curl
-
 apt-get remove -y curl
 
 # Install ipyrad
