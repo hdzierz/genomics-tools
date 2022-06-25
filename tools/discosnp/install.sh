9bd059ae490155f0b4310d6f6d7201a9383241a1
Rob Elshire <rob@elshiregroup.co.nz>
2022-06-26 10:39:49 +1200

    Removed BWA as it is brought in by installing discosnp. Fixed directory for the tree command.

--- tools/discosnp/install.sh
+++ tools/discosnp/install.sh
@@ -3,7 +3,7 @@
 # This program is to set up a docker image for the discosnp 2.6.2.
 # It assumes a Debian 11 base image has been used for the container.
 
-# Update package database and install wget and bwa (for mapping).
+# Update package database and install wget.
 
 apt-get update
 apt-get install -y wget \
@@ -18,6 +18,7 @@ bash Miniconda3-py39_4.12.0-Linux-x86_64.sh -b -p /opt/conda
 
 # Remove wget
 apt-get remove -y wget
+apt-get autoremove -y
 
 # Install discosnp via conda
 
@@ -25,4 +26,4 @@ apt-get remove -y wget
 
 # let's see what is in the installed subdirectory
 
-tree -R /opt/conda/condabin/conda/
+tree -R /opt/conda/bin/
