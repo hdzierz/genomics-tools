e03bec6832f02d30deca1fb6d4a381b145334b2c
Rob Elshire <rob@elshiregroup.co.nz>
2022-07-10 15:53:57 +1200

    Changed the test to look for batch_trim.pl

--- tools/gbs-preprocess/test.sh
+++ tools/gbs-preprocess/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Try batch_trim.pl  and see if it is returns prepared_keyfile.
+# Try batch_trim.pl  and see if it is returns batch_trim.pl.
 # If it does, then output GFANZ_TEST_RESULTS=OK. If it does not, then output
 # GFANZ_TEST_RESULTS=ERROR.
-/opt/gbs-preprocess/batch_trim.pl | grep prepared_keyfile >> /dev/null
+/opt/gbs-preprocess/batch_trim.pl | grep batch_trim.pl >> /dev/null
 
 if [ $? -eq 0 ]; then
   echo "GFANZ_TEST_RESULTS=OK"
