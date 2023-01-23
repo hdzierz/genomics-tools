fd068139d1bceae99cc4c8df6f0076033e359b8b
Rob Elshire <rob@elshiregroup.co.nz>
2023-01-24 10:16:01 +1300

    changed cat to ls based on Helge's question.

--- tools/gbs-preprocess/test.sh
+++ tools/gbs-preprocess/test.sh
@@ -3,7 +3,7 @@
 # Try batch_trim.pl  and see if it is returns batch_trim.pl.
 # If it does, then output GFANZ_TEST_RESULTS=OK. If it does not, then output
 # GFANZ_TEST_RESULTS=ERROR.
-cat /opt/gbs-preprocess/batch_trim.pl | grep batch_trim.pl >> /dev/null
+ls /opt/gbs-preprocess/batch_trim.pl | grep batch_trim.pl >> /dev/null
 
 if [ $? -eq 0 ]; then
   echo "GFANZ_TEST_RESULTS=OK"
