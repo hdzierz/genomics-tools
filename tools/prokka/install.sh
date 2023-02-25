7594ad0de9703b8280aed3cff9459e6ddf0f49fe
Rob Elshire <rob@elshiregroup.co.nz>
2023-02-25 16:23:20 +1300

    Added initial upgrade step to get security updates at build time. Added a rubbish collection step to remove unneeded files after installation.

--- tools/prokka/install.sh
+++ tools/prokka/install.sh
@@ -3,9 +3,18 @@
 # This program is to set up a docker image for prokka
 # It assumes a Debian 10 base image has been used for the container.
 
-# install the dependencies and then remove apt package related stuff to save space.
-apt-get install -y libdatetime-perl libxml-simple-perl libdigest-md5-perl git default-jre bioperl build-essential\
-	&& rm -rf /var/lib/apt/lists/*
+apt-get update
+apt-get upgrade -y
+
+# install the dependencies.
+apt-get install -y\
+    libdatetime-perl\
+    libxml-simple-perl\
+    libdigest-md5-perl\
+    git\
+    default-jre\
+    bioperl\
+    build-essential
 
 # configure cpan to work without interaction
 (echo y;echo o conf prerequisites_policy follow;echo o conf commit)|cpan
@@ -18,3 +27,9 @@ git clone https://github.com/tseemann/prokka.git /opt/prokka
 
 # Set up prokka databases
 /opt/prokka/bin/prokka --setupdb
+
+# Collect rubbish
+apt-get remove -y\
+    git\
+    build-essential
+apt-get autoremove -y
