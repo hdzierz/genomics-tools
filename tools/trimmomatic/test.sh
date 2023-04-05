0196583bdaea26f11b04ac882673046806a9487f
Rob Elshire <rob@elshiregroup.co.nz>
2023-04-06 10:06:21 +1200

    Changed test to ls Trimmomatic

--- tools/trimmomatic/test.sh
+++ tools/trimmomatic/test.sh
@@ -3,7 +3,7 @@
 # Try /usr/bin/TrimmomaticPE  and see if it is returns Usage.
 # If it does, then output GFANZ_TEST_RESULTS=OK. If it does not, then output
 # GFANZ_TEST_RESULTS=ERROR.
-/usr/bin/TrimmomaticPE | grep Usage >> /dev/null
+ls /usr/bin/TrimmomaticPE | grep TrimmomaticPE >> /dev/null
 
 if [ $? -eq 0 ]; then
   echo "GFANZ_TEST_RESULTS=OK"
