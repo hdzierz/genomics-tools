29f79340e0f0eb85e0f726116e9e2cabb5d19480
Rob Elshire <rob@elshiregroup.co.nz>
2023-04-07 12:52:34 +1200

    First commit for SolexaQA++.

---
+++ tools/solexaqa++/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This program is to set up a docker image for the SolexaQA++ version 3.1.7.3
+# It assumes a Debian 11 base image has been used for the container.
+
+# Update image
+apt-get update
+apt-get -y upgrade
+
+# Install dependencies for graphics for reports and curl to download SolexaQA++
+apt-get -y install\ 
+    r-cran-gridbase\
+    curl
+
+# install SolexaQA++
+curl https://versaweb.dl.sourceforge.net/project/solexaqa/src/SolexaQA%2B%2B_v3.1.7.3.zip -o /tmp/SolexaQA%2B%2B_v3.1.7.3.zip
+cd /tmp
+unzip SolexaQA%2B%2B_v3.1.7.3.zip
+mv /tmp/SolexaQA%2B%2B_v3.1.7.3/Linux_x64/SolexaQA++ /opt/
+
+# Clean up - Remove any packages / files needed only to build the image (e.g. build-essential) and clean the apt cache files.
+apt-get remove --purge curl
+apt-get clean
